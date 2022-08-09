3abdb14f3e628d235e074006e80a971b71e990dc
danilobml <danbarml@gmail.com>
2022-08-09 13:41:15 +0200

    create user route modified

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,18 @@ export const get_logged_user = async (req, res) => {
   }
 };
 
+export const user_check = async (req, res) => {
+  const { email } = req.body;
+
+  const exists = await User.findOne({ email: email });
+
+  if (exists) {
+    res.send("invalid");
+  } else {
+    res.status(200).send(email);
+  }
+};
+
 export const get_other_users = async (req, res) => {
   const { email } = req.params;
   try {
@@ -61,7 +73,7 @@ export const create_new_user = async (req, res) => {
 
     const token = newUser.createToken();
 
-    res.set("x-authorization-token", token).json({ _id: newUser._id, email: newUser.email });
+    res.send({ email, token });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -74,6 +86,20 @@ export const partially_update_user = async (req, res) => {
     const { modifiedCount } = await User.updateOne({ _id: id }, { [key]: value });
     if (!modifiedCount) return res.status(404).send("user not found");
     res.send("The user was updated successfully");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const fully_update_user = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedUser) return res.status(404).send("No such user");
+    res.json(updatedUser);
   } catch (error) {
     console.log(error.message);
   }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 export const userRouter = express.Router();
-import { get_all_users, get_logged_user, get_other_users, create_new_user, partially_update_user } from "../controllers/userController.js";
+import { get_all_users, get_logged_user, get_other_users, create_new_user, partially_update_user, fully_update_user, user_check } from "../controllers/userController.js";
 
 import { authorizeUser } from "../middlewares/authorizeUser.js";
 
 userRouter.get("/", get_all_users);
+
+userRouter.post("/check", user_check);
 
 userRouter.get("/logged_user/:email", get_logged_user);
 
@@ -13,3 +15,5 @@ userRouter.get("/:email", get_other_users);
 userRouter.post("/create", create_new_user);
 
 userRouter.patch("/:id", partially_update_user);
+
+userRouter.put("/:id", fully_update_user);
